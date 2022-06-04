6da3d78bb5a28cfa0ae89cc391106e71c2139c39
Ronan Bastos <23327501+ronanbastos@users.noreply.github.com>
2022-06-03 22:18:33 -0300

    Add files via upload

---
+++ Demonstration/jogo da velha/logicaO.js
@@ -0,0 +1,85 @@
+vitoiraO=false
+
+owin1=[
+    "camada0/o",
+    "camada3/o",
+    "camada6/o"
+]
+
+owin2=[
+    "camada1/o",
+    "camada4/o",
+    "camada7/o"
+]
+owin3=[
+    "camada2/o",
+    "camada5/o",
+    "camada8/o"
+]
+
+owin4=[
+    "camada0/o",
+    "camada1/o",
+    "camada2/o"
+]
+owin5=[
+    "camada3/o",
+    "camada4/o",
+    "camada5/o"
+]
+owin6=[
+    "camada6/o",
+    "camada7/o",
+    "camada8/o"
+]
+owin7=[
+    "camada6/o",
+    "camada4/o",
+    "camada2/o"
+]
+
+owin8=[
+    "camada0/o",
+    "camada4/o",
+    "camada8/o"
+]
+
+function fimWinO(){
+
+    if(mapsO.includes(owin1[0])==true && mapsO.includes(owin1[1])==true && mapsO.includes(owin1[2])==true){
+      
+       alert("PLayer O ganhou!")
+    }
+    
+    if(mapsO.includes(owin2[0])==true && mapsO.includes(owin2[1])==true && mapsO.includes(owin2[2])==true){
+
+       alert("PLayer O ganhou!")
+    }
+    if(mapsO.includes(owin3[0])==true && mapsO.includes(owin3[1])==true && mapsO.includes(owin3[2])==true){
+
+       alert("PLayer O ganhou!")
+    }
+    if(mapsO.includes(owin4[0])==true && mapsO.includes(owin4[1])==true && mapsO.includes(owin4[2])==true){
+
+       alert("PLayer O ganhou!")
+    }
+    if(mapsO.includes(owin5[0])==true && mapsO.includes(owin5[1])==true && mapsO.includes(owin5[2])==true){
+
+       alert("PLayer O ganhou!")
+        
+    }
+    if(mapsO.includes(owin6[0])==true && mapsO.includes(owin6[1])==true && mapsO.includes(owin6[2])==true){
+
+           alert("PLayer O ganhou!")
+    }
+    if(mapsO.includes(owin7[0])==true && mapsO.includes(owin7[1])==true && mapsO.includes(owin7[2])==true){
+
+       alert("PLayer O ganhou!")
+    }
+    if(mapsO.includes(owin8[0])==true && mapsO.includes(owin8[1])==true && mapsO.includes(owin8[2])==true){
+
+       alert("PLayer O ganhou!")
+    }
+
+
+}
---
+++ Demonstration/jogo da velha/logicaX.js
@@ -0,0 +1,83 @@
+vitoiraX=false
+
+xwin1=[
+    "camada0/x",
+    "camada3/x",
+    "camada6/x"
+]
+
+xwin2=[
+    "camada1/x",
+    "camada4/x",
+    "camada7/x"
+]
+xwin3=[
+    "camada2/x",
+    "camada5/x",
+    "camada8/x"
+]
+
+xwin4=[
+    "camada0/x",
+    "camada1/x",
+    "camada2/x"
+]
+xwin5=[
+    "camada3/x",
+    "camada4/x",
+    "camada5/x"
+]
+xwin6=[
+    "camada6/x",
+    "camada7/x",
+    "camada8/x"
+]
+xwin7=[
+    "camada6/x",
+    "camada4/x",
+    "camada2/x"
+]
+
+xwin8=[
+    "camada0/x",
+    "camada4/x",
+    "camada8/x"
+]
+
+function fimWinX(){
+    if(mapsX.includes(xwin1[0])==true && mapsX.includes(xwin1[1])==true && mapsX.includes(xwin1[2])==true){
+      
+        alert("player x ganhou!")
+    }
+    
+    if(mapsX.includes(xwin2[0])==true && mapsX.includes(xwin2[1])==true && mapsX.includes(xwin2[2])==true){
+
+        alert("player x ganhou!")
+    }
+    if(mapsX.includes(xwin3[0])==true && mapsX.includes(xwin3[1])==true && mapsX.includes(xwin3[2])==true){
+
+        alert("player x ganhou!")
+    }
+    if(mapsX.includes(xwin4[0])==true && mapsX.includes(xwin4[1])==true && mapsX.includes(xwin4[2])==true){
+
+        alert("player x ganhou!")
+    }
+    if(mapsX.includes(xwin5[0])==true && mapsX.includes(xwin5[1])==true && mapsX.includes(xwin5[2])==true){
+
+        alert("player x ganhou!")
+        
+    }
+    if(mapsX.includes(xwin6[0])==true && mapsX.includes(xwin6[1])==true && mapsX.includes(xwin6[2])==true){
+
+            alert("player x ganhou!")
+    }
+    if(mapsX.includes(xwin7[0])==true && mapsX.includes(xwin7[1])==true && mapsX.includes(xwin7[2])==true){
+
+        alert("player x ganhou!")
+    }
+    if(mapsX.includes(xwin8[0])==true && mapsX.includes(xwin8[1])==true && mapsX.includes(xwin8[2])==true){
+
+        alert("player x ganhou!")
+    }
+
+}
