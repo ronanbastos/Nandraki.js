cc5a3b07ee15f727e4b6dfed09e4d2ed1ed4f949
Ronan Bastos <23327501+ronanbastos@users.noreply.github.com>
2022-07-27 23:53:28 -0300

    Update nandraki.js

--- nandraki.js
+++ nandraki.js
@@ -957,6 +957,10 @@ game = {
         
 
     },
+    in$string: function(string1,string2){
+
+        return string1.includes(string2) == true
+    },
     block_all: function (selector) {
 
         let myObj = document.querySelectorAll(selector),
